7c6ef6df2b09a08d3cc162b9b2ff3f723ec62ab4
Karlo Eleuterio <karloe@gmail.com>
2019-05-06 00:38:33 -0400

    rename path

--- functions/index.js
+++ functions/index.js
@@ -2,4 +2,4 @@ const functions = require('firebase-functions');
 const preferences = require('./controllers/preferences.js');
 
 // Expose Express APIs as a single Cloud Function:
-exports.testRecipes = functions.https.onRequest(preferences);
+exports.preferences = functions.https.onRequest(preferences);
