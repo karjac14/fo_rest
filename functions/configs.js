a1e6c8dde1eb69e3a0d78e306085e11b78445fc3
Karlo Eleuterio <karloe@gmail.com>
2019-05-15 10:23:44 -0400

    [devops]: remove config file from ignore list

---
+++ functions/configs.js
@@ -0,0 +1,8 @@
+
+
+exports.spoonUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex';
+
+exports.spoonConfigs = {
+    "X-RapidAPI-Host": 'UPDATE ME',
+    "X-RapidAPI-Key": 'UPDATE ME'
+};
