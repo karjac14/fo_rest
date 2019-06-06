cd30bcc2b7661caf3920e1d45fcaed95b5fc7f03
Karlo Eleuterio <karloe@gmail.com>
2019-06-06 00:19:46 -0400

    scafold get mthod for groceries endpoint

---
+++ functions/controllers/groceries.js
@@ -0,0 +1,108 @@
+// const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+const configs = require('../configs-active.js');
+
+const app = express();
+app.use(cors({ origin: true }));
+
+// TODO: Add middleware to authenticate requests
+
+// Spoonacular Get APIs
+const spoonUrlInfoBulk = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk';
+const spoonHeader = configs.spoonConfigs;
+
+
+//THIS CONTROLLER HANDLES RECIPE SUGGESTIONS
+//Methods: Get, Post
+app.get('/', (req, res) => {
+
+    let week = req.query.week;
+    let year = req.query.year;
+    let firstDay = req.query.firstDay;
+    let lastDay = req.query.lastDay;
+
+    let docId = `${week}-${year}-${req.query.uid}`;
+
+
+    var db = admin.firestore();
+    var grocRef = db.collection("user_groceries");
+    var suggestionsRef = db.collection("user_suggestions");
+
+    let ingredients = [];
+
+    let getBreakChainError = () => {
+        let err = new Error();
+        err.name = 'BreackChainError';
+        return err;
+    };
+
+    grocRef.doc(docId).get()
+        .then((doc) => {
+            if (doc.exists) {
+                let data = doc.data();
+                res.status(200).json(data);
+                throw getBreakChainError(); //terminate the chain early
+            } else {
+                return suggestionsRef.doc(docId).get()
+            }
+        })
+        .then((doc) => {
+            if (doc.exists) {
+                let data = doc.data();
+                let meals = data.suggestions.filter(x => x.selected);
+                let ids = [];
+                meals.forEach((el) => {
+                    ids.push(el.id)
+                })
+                return ids.join()
+            } else {
+                res.status(200).json({ noSuggestions: true, week, year })
+                throw getBreakChainError(); //terminate the chain early
+            }
+        })
+        .then((idsStr) => {
+
+            let spoonData = {
+                ids: idsStr
+            };
+            let spoonConfig = {
+                headers: spoonHeader,
+                params: spoonData
+            };
+            return axios.get(spoonUrlInfoBulk, spoonConfig)
+        })
+        .then(response => {
+
+            response.data.forEach(meal => {
+                Array.prototype.push.apply(ingredients, meal.extendedIngredients);
+            });
+            ingredients.forEach((el, i) => {
+                ingredients[i].fridge = false;
+                ingredients[i].cart = false;
+                ingredients[i].weekIngId = i;
+            });
+
+            return grocRef.doc(docId).set({ ingredients, week, year })
+        })
+        .then((doc) => {
+            return res.status(200).json({ ingredients, week, year });
+        })
+        .catch((err) => {
+            if (err.name !== 'BreackChainError') {
+                res.status(500).json({
+                    error: err
+                });
+            }
+        });
+});
+
+app.patch('/', (req, res) => {
+    // TODO make this patch work
+    res.status(200).json({ success: true });
+});
+
+
+module.exports = app
